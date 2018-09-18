b0420f15d9f88b6cf64c6e502b26dcd50d8f5fce
DarlyngSandoval <darlyngg12.05@hotmail.es>
2018-09-18 12:49:51 -0500

    Migraciones

---
+++ app/Faculty.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    //
+    protected $fillable = [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+    ];
+}
